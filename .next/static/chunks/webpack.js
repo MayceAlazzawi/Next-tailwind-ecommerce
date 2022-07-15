4b8052a49dc6293e2a40aeffe0f35419888cd8d1
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-15 23:51:28 +0300

    add json again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89805539253dc2e1"; }
+/******/ 		__webpack_require__.h = function() { return "44b0731568a2eae8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/product/[slug].js
@@ -0,0 +1,20 @@
+import data from '../utils/data'
+import { useRouter } from 'next/router';
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const productScreen = () => {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find(x => x.slug === slug)
+  if (!product) {
+    return <div>Product Not Found</div>
+  }
+  return (
+    <Layout title={product.name}>
+      {product.name}
+    </Layout>
+  )
+}
+
+export default productScreen
