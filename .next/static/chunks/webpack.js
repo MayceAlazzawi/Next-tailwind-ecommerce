d412e7a4fa8e64004244f308844f99fa471b0f20
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-17 19:44:39 +0300

    adding product details page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e887d716f426c677"; }
+/******/ 		__webpack_require__.h = function() { return "11d62c2a3461f733"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
